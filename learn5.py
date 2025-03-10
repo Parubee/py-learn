d23406c9dda62086846fb21d61a2981598854eea
Paravthi KB <127673281+Parubee@users.noreply.github.com>
2025-03-10 15:03:22 +0530

    Add files via upload

---
+++ learn5.py
@@ -0,0 +1,9 @@
+
+#program to convert time in sec to HH:MM:SS format
+time=input("Enter time in seconds")
+time=int(time)
+timeinmin=time//60
+timeinsec=time%60
+timeinhr=timeinmin//60
+timeinmin=timeinmin%60
+print("HH:MM::SS----{}:{}:{}".format(timeinhr,timeinmin,timeinsec))
