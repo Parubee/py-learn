e8cb2b46ee4e541514903d73937854fdf0db5008
Paravthi KB <127673281+Parubee@users.noreply.github.com>
2025-03-10 15:15:50 +0530

    Create learn6.py

---
+++ learn6.py
@@ -0,0 +1,6 @@
+import math
+r = int(input("Enter the radius of your circle:"))
+area = math.pi *r*r
+circum = math.pi *r*2
+print(f"Your area for {r} is {area}")
+print(f"Your perimeter for {r} is {circum}")
